0d00049138578c0bd7011f382f82e3f08cec923c
Kartshopcom <ml@kartshop.com>
2026-06-12 07:38:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -94,14 +94,13 @@ app.get("/forsendelser", async (req, res) => {
   }
 });
 
-// DanDomain: hent produkter via GetByModifiedInterval
+// DanDomain: hent produkter via ByModificationDate
 app.get("/produkter", async (req, res) => {
   const { key, page, size } = req.query;
   const siteId = 26;
   const pageSize = size || 100;
   const pageIndex = page || 1;
-  // Hent produkter modificeret siden år 2000 = alle produkter
-  const url = "http://otkshop.dk/admin/webapi/Endpoints/v1_0/ProductService/" + key + "/GetByModifiedInterval/" + siteId + "?modifiedAfter=2000-01-01T00:00:00&pageIndex=" + pageIndex + "&pageSize=" + pageSize;
+  const url = "http://otkshop.dk/admin/webapi/Endpoints/v1_0/ProductService/" + key + "/ByModificationDate/2000-01-01T00:00:00/" + siteId + "?pageIndex=" + pageIndex + "&pageSize=" + pageSize;
   try {
     const r = await fetch(url, { headers: { Accept: "application/json" } });
     const text = await r.text();
